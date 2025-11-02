77d61fed6b55c15174710d2efd5ddbf1abf4256f
devBerringer <blakeberringer@gmail.com>
2025-11-01 22:37:31 -0500

    removed arrow pointing down on hero

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -117,23 +117,6 @@ export default function Hero() {
             ))}
           </motion.div>
         </div>
-
-        {/* Scroll Indicator */}
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.5, delay: 1 }}
-          className="absolute bottom-8 left-1/2 transform -translate-x-1/2"
-        >
-          <motion.div
-            animate={{ y: [0, 10, 0] }}
-            transition={{ duration: 2, repeat: Infinity }}
-            className="text-gray-400 cursor-pointer"
-            onClick={() => document.querySelector('#about')?.scrollIntoView({ behavior: 'smooth' })}
-          >
-            <FiArrowDown size={24} />
-          </motion.div>
-        </motion.div>
       </div>
     </section>
   );
